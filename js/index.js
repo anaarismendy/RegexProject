e9227a6c8e599eee9b08ac1ea84b6db60131af82
Mateo Monsalve <mateo_monsalve75222@elpoli.edu.co>
2025-03-20 16:00:20 -0500

    inicio de validaciones en js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let cedula, nombres, apellidos, nombre_usuario, celular, email, direccion, fecha_nacimiento, contrasena, confirmar_contrasena;
+window.onload = function(){
+    cedula = document.getElementById("cedula");
+    nombres = document.getElementById("nombre");
+    apellidos = document.getElementById("apellidos");
+    nombre_usuario = document.getElementById("nombre_usuario");
+    celular = document.getElementById("celular");
+    email = document.getElementById("email");
+    direccion = document.getElementById("direccion");
+    fecha_nacimiento = document.getElementById("fecha_nacimiento");
+    contrasena = document.getElementById("contrasena");
+    confirmar_contrasena = document.getElementById("confirmar_contrasena");
+
+}
